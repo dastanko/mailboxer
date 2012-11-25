018dda573291da0b8906f714266d8d7258c251b7
Dastan Kojomuratov <dastanko089@gmail.com>
2012-11-25 17:19:28 +0600

    Try #3

--- db/migrate/20121125110946_create_message_attachments.rb
+++ db/migrate/20121125110946_create_message_attachments.rb
@@ -2,10 +2,10 @@ class CreateMessageAttachments < ActiveRecord::Migration
   def change
     create_table :message_attachments do |t|
       t.string :attachment
-      t.references :notification
+      t.references :message
 
       t.timestamps
     end
-    add_index :message_attachments, :notification_id
+    add_index :message_attachments, :message_id
   end
 end
